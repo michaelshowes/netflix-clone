893e4260ebd1a65adb6eb05ae64ab798c5b5763f
Michael Showes <michael.showes@gmail.com>
2023-03-03 04:29:46 -0500

    removed starter code and added some config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true
+	},
+	// Enables support for importing SVGs as components
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack']
+		});
+		return config;
+	},
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		remotePatterns: [
+			{
+				hostname: 'localhost'
+			}
+		],
+		domains: []
+	},
+	sassOptions: {
+		prependData: `
+			@use "~@styles/functions" as *;
+			@use "~@styles/mixins" as *;
+			@use "~@styles/global/colors.scss" as *;
+			@use "~@styles/global/settings.scss" as *;
+		`
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

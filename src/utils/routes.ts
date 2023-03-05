b4a28c221998cd67d286445e127d549c47850ddb
Michael Showes <michael.showes@gmail.com>
2023-03-04 23:51:53 -0500

    added routes

---
+++ src/utils/routes.ts
@@ -0,0 +1,22 @@
+export const routes = [
+	{
+		name: 'Home',
+		path: '/'
+	},
+	{
+		name: 'TV Shows',
+		path: '/tv-shows'
+	},
+	{
+		name: 'Movies',
+		path: '/movies'
+	},
+	{
+		name: 'Latest',
+		path: '/latest'
+	},
+	{
+		name: 'My List',
+		path: '/my-list'
+	}
+];

9544fa131aa29328f9982865588234ef10654e97
Michael Showes <michael.showes@gmail.com>
2023-03-04 23:54:30 -0500

    added hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+// Hero Component
+
+import Image from 'next/image';
+import scss from './hero.module.scss';
+
+type HeroProps = {};
+
+export default function Hero({}: HeroProps) {
+	return (
+		<div className={scss.hero}>
+			<div className={scss.image}>
+				<Image />
+			</div>
+		</div>
+	);
+}
